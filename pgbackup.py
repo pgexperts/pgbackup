8686f861284220c266d994432bf1638b0ab7b7fa
Jeff Frost <jeff@pgexperts.com>
2012-05-03 17:32:21 -0700

    PEP changes and use os.getenv() instead of os.environ[]

--- pgbackup.py
+++ pgbackup.py
@@ -11,13 +11,20 @@ from optparse import OptionParser
 
 parser = OptionParser()
 
-parser.add_option('-a', "--alertemail", action="store", type="string", dest="alertemail", default='admin@pgexperts.com')
-parser.add_option('-d', "--destdir", action="store", type="string", dest="backupdir", default='/data/pg/backup/dumps')
-parser.add_option('-k', "--keep", action="store", type="int", dest="keep", default=7)
-parser.add_option('', "--pghost", action="store", type="string", dest="pghost")
-parser.add_option('-U', "--pguser", action="store", type="string", dest="pguser")
-parser.add_option('-p', "--port", action="store", type="string", dest="pgport")
-parser.add_option('-c', "--compress", action="store", type="int", dest="compress", default=6)
+parser.add_option('-a', "--alertemail", action="store", type="string",
+                  dest="alertemail", default='admin@pgexperts.com')
+parser.add_option('-d', "--destdir", action="store", type="string",
+                  dest="backupdir", default='/data/pg/backup/dumps')
+parser.add_option('-k', "--keep", action="store", type="int", dest="keep",
+                  default=7)
+parser.add_option('', "--pghost", action="store", type="string",
+                  dest="pghost")
+parser.add_option('-U', "--pguser", action="store", type="string",
+                  dest="pguser")
+parser.add_option('-p', "--port", action="store", type="string",
+                  dest="pgport")
+parser.add_option('-c', "--compress", action="store", type="int",
+                  dest="compress", default=6)
 
 (options, args) = parser.parse_args()
 
@@ -25,14 +32,14 @@ parser.add_option('-c', "--compress", action="store", type="int", dest="compress
 # from the environment or from reasonable defaults
 dbenv = {"PATH": os.environ["PATH"]}
 
-if options.pghost or os.environ["PGHOST"]:
-    dbenv["PGHOST"] = options.pghost or os.environ["PGHOST"]
+if options.pghost or os.getenv("PGHOST"):
+    dbenv["PGHOST"] = options.pghost or os.getenv("PGHOST")
 
 dbenv["PGPORT"] = options.pgport or os.getenv("PGPORT") or "5432"
 dbenv["PGUSER"] = options.pguser or os.getenv("PGUSER") or "postgres"
 
 # hostname used for file naming convention
-dbhost = options.pghost or ( os.environ["PGHOST"] or socket.getfqdn() )
+dbhost = options.pghost or os.getenv("PGHOST") or socket.getfqdn()
 print dbhost
 print dbenv
 
@@ -40,13 +47,17 @@ globalsfile = dbhost + '-globals-' + strftime('%Y%m%d') + '.sql.bz2'
 print globalsfile
 
 globals_out = bz2.BZ2File(globalsfile, 'wb')
-globals_in =  subprocess.Popen(["pg_dumpall", "--globals"], stdout=subprocess.PIPE, env=dbenv)
+globals_in =  subprocess.Popen(["pg_dumpall", "--globals"],
+    stdout=subprocess.PIPE, env=dbenv)
 globals_out.writelines(globals_in.stdout)
 globals_out.close()
 
-databases = subprocess.check_output(["psql", "--no-align", "--tuples-only", "--command", "SELECT datname FROM pg_database WHERE datname NOT IN ('postgres','template0','template1')","postgres"], env=dbenv)
+databases = subprocess.check_output(["psql", "--no-align", "--tuples-only",
+    "--command",
+    "SELECT datname FROM pg_database WHERE datname NOT IN ('postgres',"
+    "'template0','template1')","postgres"], env=dbenv)
 
 for db in databases.split():
-   print db
+    print db
 
 
