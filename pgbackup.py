ccb109b7215cdf0825aaf68346e5eaf3e80a51cd
Jeff Frost <jeff@frostconsultingllc.com>
2012-05-22 14:54:12 -0700

    license, cleanup old dump files and PEP

--- pgbackup.py
+++ pgbackup.py
@@ -1,7 +1,28 @@
 #!/bin/env python
 # -*- coding: utf-8 *-*
+#
+#    pgbackup.py is licensed under the PostgreSQL License:
+#
+#    Copyright (c) 2012, PostgreSQL, Experts, Inc.
+#
+#    Permission to use, copy, modify, and distribute this software and its
+#    documentation for any purpose, without fee, and without a written
+#    agreement is hereby granted, provided that the above copyright notice and
+#    this paragraph and the following two paragraphs appear in all copies.
+#
+#    IN NO EVENT SHALL POSTGRESQL EXPERTS, INC. BE LIABLE TO ANY PARTY FOR
+#    DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING
+#    LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS
+#    DOCUMENTATION, EVEN IF POSTGRESQL EXPERTS, INC. HAS BEEN ADVISED OF THE
+#    POSSIBILITY OF SUCH DAMAGE.
+#
+#    POSTGRESQL EXPERTS, INC. SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING,
+#    BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+#    FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS ON AN "AS IS"
+#    BASIS, AND POSTGRESQL EXPERTS, INC. HAS NO OBLIGATIONS TO PROVIDE
+#    MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 
-from time import strftime
+from time import strftime,time
 import tempfile
 import bz2
 import os
@@ -52,7 +73,8 @@ logfile = tempfile.TemporaryFile()
 
 globalsfile = options.backupdir + '/' + dbhost + '-globals-'\
   + strftime('%Y%m%d') + '.sql.bz2'
-if options.debug: print "GLOBALSFILE: " + globalsfile
+if options.debug:
+    print "GLOBALSFILE: " + globalsfile
 
 globals_out = bz2.BZ2File(globalsfile, 'wb')
 globals_in = subprocess.Popen(["pg_dumpall", "--globals"],
@@ -60,25 +82,42 @@ globals_in = subprocess.Popen(["pg_dumpall", "--globals"],
 globals_out.writelines(globals_in.stdout)
 globals_out.close()
 
+# Let's get a list of databases to dump
 databases = subprocess.check_output(["psql", "--no-align", "--tuples-only",
     "--command",
     "SELECT datname FROM pg_database WHERE datname NOT IN ('postgres', "
     "'template0','template1')", "postgres"], env=dbenv)
 
-
+# Now that we've got a list, let's get to work
 for db in databases.split():
-    if options.debug: print "DB: " + db
+    if options.debug:
+        print "DB: " + db
     dbfile = options.backupdir + '/' + dbhost + '-' + db + '-'\
       + strftime('%Y%m%d') + '.dmp'
-    if options.debug: print "DBFILE: " + dbfile
+    if options.debug:
+        print "DBFILE: " + dbfile
     pg_dump = ['pg_dump', '--format=custom',
         '--compress=' + str(options.compress),
         '--file=' + dbfile, db]
-    if options.debug: print "PG_DUMP: " + str(pg_dump)
-    dumpreturn = subprocess.call(pg_dump, stderr=logfile, env=dbenv)
-    if dumpreturn != 0:
+    if options.debug:
+        print "PG_DUMP: " + str(pg_dump)
+    dumpreturnvalue = subprocess.call(pg_dump, stderr=logfile, env=dbenv)
+    if dumpreturnvalue != 0:
         failed = 1
 
+#Cleanup old backups
+now = time()
+for dumpfile in os.listdir(options.backupdir):
+    if os.stat(os.path.join(options.backupdir, dumpfile)).st_mtime\
+      < now - options.keep * 86400:
+        if os.path.isfile(os.path.join(options.backupdir, dumpfile)):
+            try:
+                os.remove(os.path.join(options.backupdir, dumpfile))
+            except:
+                print "Error deleting " + dumpfile
+
+# Print out the logfile which contains stderr from pg_dump
+# if we failed
 if failed:
     print "dump failed"
     logfile.seek(0)
